a3a101fb56e55eb73fdd737c2dea42ba768a9d13
BuildTools <unconfigured@null.spigotmc.org>
2021-05-12 05:18:51 +0200

    Update Documentation

--- Player/DiscordVoice.cs
+++ Player/DiscordVoice.cs
@@ -9,21 +9,47 @@ using Newtonsoft.Json.Linq;
 
 namespace discordVoicePlayer.Player
 {
+    /**
+     * <summary>
+     * Class <c>DiscordVoice</c> provides a light Management about Invitations.
+     * </summary>
+     */
     public class DiscordVoice
     {
 
         private readonly HttpClient _httpClient = new();
 
+        /**
+         * <summary>
+         * The Ctor <c>DiscordVoice</c> is for creating a new Management
+         * corresponding to the Token.
+         *
+         * <param name="token">A <c>string</c> which can be a Bot- or an User-Token as well</param>
+         * <param name="bot">A <c>bool</c> which is the State set to true when the Token relates to a Bot</param>
+         * </summary>
+         */
         public DiscordVoice(string token, bool bot)
         {
             var requestHeaders = this._httpClient.DefaultRequestHeaders;
             requestHeaders.Add("Authorization", bot ? $"Bot {token}" : token);
         }
 
+        /**
+         * <summary>
+         * The Method <c>CreateInvite</c> creates an Invite with the associated VoiceMode.
+         *
+         * <param name="channelSnowflake">A <c>string</c> which serves as Id of a Voice-Channel to create the Invitation at</param>
+         * <param name="voiceMode">A <c>string</c> which serves as the VoiceMode to the corresponding Voice-Channel</param>
+         *
+         * <returns>Returns an asynchronous <c>Task of a string</c> which contains the Invitation-Code, when everything was done</returns>
+         * </summary>
+         */
         public async Task<string> CreateInvite(string channelSnowflake, string voiceMode)
         {
             if (!(DiscordVoiceMode.Contains(voiceMode)) || !(await this.IsChannelVoice(channelSnowflake)))
                 return "is not a voice channel or voiceMode not valid";
+            
+            
             
             var inviteEntity = CreateInviteEntity(0, 0, 2, false, voiceMode);
             var stringContent = new StringContent(JsonConvert.SerializeObject(inviteEntity),
--- Player/DiscordVoiceMode.cs
+++ Player/DiscordVoiceMode.cs
@@ -1,5 +1,9 @@
 namespace discordVoicePlayer.Player
 {
+    
+    /**
+     * <summary>Class <c>DiscordVoiceMode</c> provides all the possible VoiceModes.</summary>>
+     */
     public static class DiscordVoiceMode
     {
 
@@ -8,7 +12,10 @@ namespace discordVoicePlayer.Player
         public static string FishingtonIo => "814288819477020702";
         public static string PokerNight => "755827207812677713";
 
-        public static bool Contains(string voiceMode)
+        /**
+         * <summary>Method <c>Contains</c> checks if any available VoiceMode is included.</summary>
+         */
+        internal static bool Contains(string voiceMode)
         {
             return FishingtonIo.Equals(voiceMode)
                    || BetrayIo.Equals(voiceMode)
